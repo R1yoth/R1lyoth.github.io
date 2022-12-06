ff28c3aebc7875f5f74146d979174d45eed9d91e
R1yoth <mayke1212master@gmail.com>
2022-12-06 13:42:49 -0300

    new title and box changes

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+//script files are in here(ooh who would have thought, captain obvious...)
+
+function websiteVisits(response){
+    document.querySelector("#visits").textContent = response.value;
+}
