3efaf8e82bc134474882ef7121eb3823cb3d5105
R1yoth <mayke1212master@gmail.com>
2023-02-22 16:31:53 -0300

    full rework

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-
-//script files are in here(ooh who would have thought, captain obvious...)
-
-function websiteVisits(response){
-    document.querySelector("#visits").textContent = response.value;
-}
